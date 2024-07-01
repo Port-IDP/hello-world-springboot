ff60f7ce0a74b52157cc07fa5869add434e23046
Sunny Goel <sunny.goel@slalom.com>
2024-06-30 19:51:04 -0700

    updated code as per styling guidelines

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String getHelloMessage() {
-        return "Hello, World!";
-    }
+  @GetMapping("/hello")
+  public String getHelloMessage() {
+    return "Hello, World!";
+  }
 }
